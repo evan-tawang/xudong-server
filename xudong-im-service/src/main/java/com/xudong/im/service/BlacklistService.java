48bb581ef8a4a57ec9004a68d2efb792a4b368c3
沈炜 <277469513@qq.com>
2019-06-12 19:56:58 +0800

    黑名单

--- xudong-im-service/src/main/java/com/xudong/im/service/BlacklistService.java
+++ xudong-im-service/src/main/java/com/xudong/im/service/BlacklistService.java
@@ -2,11 +2,13 @@ package com.xudong.im.service;
 
 import com.xudong.im.data.mapper.BlackListMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @author Evan.Shen
  * @since 2019-06-12
  */
+@Service
 public class BlacklistService {
 
     @Autowired
---
+++ xudong-im-service/src/test/java/test/com/xudong/im/service/BlacklistServiceTest.java
@@ -0,0 +1,26 @@
+package test.com.xudong.im.service;
+
+import com.xudong.im.service.BlacklistService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import test.com.xudong.im.support.ServiceTestCaseSupport;
+
+/**
+ * @author Evan.Shen
+ * @since 2019-06-12
+ */
+
+public class BlacklistServiceTest extends ServiceTestCaseSupport {
+    @Autowired
+    private BlacklistService blacklistService;
+
+    /**
+     * Method: login(StaffLoginDTO loginDto)
+     */
+    @Test
+    public void testIsBlock() {
+        Boolean result = blacklistService.isBlock("129.22.33.11");
+
+        LOGGER.info("====>>testIsBlock:" + result);
+    }
+}
