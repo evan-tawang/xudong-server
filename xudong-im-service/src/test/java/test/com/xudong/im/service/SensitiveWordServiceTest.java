13313bf1681b30eab7b6ffd1d87c4604ab03848a
沈炜 <277469513@qq.com>
2019-06-14 00:31:03 +0800

    敏感词过滤封装完成

--- xudong-im-service/src/test/java/test/com/xudong/im/service/SensitiveWordServiceTest.java
+++ xudong-im-service/src/test/java/test/com/xudong/im/service/SensitiveWordServiceTest.java
@@ -1,5 +1,6 @@
 package test.com.xudong.im.service;
 
+import com.xudong.im.manage.SensitiveWordManage;
 import com.xudong.im.service.SensitiveWordService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,20 +18,28 @@ public class SensitiveWordServiceTest extends ServiceTestCaseSupport {
     @Autowired
     private SensitiveWordService sensitiveWordService;
 
+    @Autowired
+    private SensitiveWordManage sensitiveWordManage;
+
 
     /**
      * Method: filter(String content)
      */
     @Test
     public void testFilter() {
-       String result =  sensitiveWordService.filter("傻逼吃饭了阿妈a,大傻逼");
+        String result = sensitiveWordService.filter("傻逼吃饭了阿妈a,大傻逼");
         LOGGER.info("====>>testFilter result1:" + result);
 
-        result =  sensitiveWordService.filter("aaa");
+        String content = "我的插插插";
+
+        result = sensitiveWordService.filter(content);
         LOGGER.info("====>>testFilter result2:" + result);
 
-        result =  sensitiveWordService.filter("ddd");
-        LOGGER.info("====>>testFilter result2:" + result);
+        String old = sensitiveWordManage.get();
+        sensitiveWordManage.save(old + "," + content);
+
+        result = sensitiveWordService.filter(content);
+        LOGGER.info("====>>testFilter result3:" + result);
     }
 
 } 
