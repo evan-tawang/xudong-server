43ef6c7d675d6413004999508d1a6b5a899cf338
mouhaining <>
2019-07-10 21:10:43 +0800

    cors test

--- xudong-im-service/src/main/java/com/xudong/im/config/CorsConfig.java
+++ xudong-im-service/src/main/java/com/xudong/im/config/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.addAllowedOrigin("*"); // 1
+        corsConfiguration.addAllowedOrigin("*"); // 1
         corsConfiguration.addAllowedHeader("*"); // 2
         corsConfiguration.addAllowedMethod("*"); // 3
         return corsConfiguration;
