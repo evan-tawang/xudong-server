03476731501e99927014f2909c2f5514daa0cea6
mouhaining <>
2019-07-22 17:36:56 +0800

    迁移

--- z-bin/mvn-deploy-server.bat
+++ z-bin/mvn-deploy-server.bat
@@ -1,21 +1,16 @@
 @echo off
 
-set jvm=$1
-set LOG_DIR=D:/im/workspace/build/logs/
+set jvm=-Xms512M -Xmx512M
+set LOG_DIR=D:/im/build/logs/
 set service=xudong-im-service
 
 set WORK_DIR=D:/im/workspace/xudong-server
-set BUILD_DIR=D:/im/workspace/build/
-set CONF_DIR=D:/im/conf/
+set BUILD_DIR=D:\im\build\server\
+set CONF_DIR=D:\im\conf
 
 set mvn=D:/im/support/apache-maven-3.5.3/bin/mvn.cmd
-set java=C:/Program Files/Java/jdk1.8.0_221/bin/java.exe
 
 echo Begin deploy......
-
-if [ ! -n "$jvm" ]; then
-    jvm="-Xms512M -Xmx512M"
-fi
 
 cd %WORK_DIR%
 
@@ -23,14 +18,22 @@ git pull
 
 cd %WORK_DIR%/%service%
 
-%mvn% clean deploy -Dmaven.test.skip=true
+call %mvn% clean deploy -Dmaven.test.skip=true
 
-copy target/%service%-1.0.jar %BUILD_DIR%/server/
-copy %CONF_DIR%/application.yml %BUILD_DIR%/server/
+rd /s /q %BUILD_DIR%
 
-echo "java %jvm% -DLOG_DIR=%LOG_DIR% -jar %service%-1.0.jar >/dev/null &"
+xcopy target\%service%-1.0.jar %BUILD_DIR%
+xcopy %CONF_DIR%\application.yml %BUILD_DIR%
 
-nohup %java% %jvm% -DLOG_DIR=%LOG_DIR% -jar %service%-1.0.jar >/dev/null &
+cd %BUILD_DIR%
 
-cmd
+echo "java %jvm% -DLOG_DIR=%LOG_DIR% -jar %service%-1.0.jar"
+
+java %jvm% -DLOG_DIR=%LOG_DIR% -jar %service%-1.0.jar
+
+
+
+
+
+
 
